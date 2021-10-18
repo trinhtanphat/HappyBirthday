f5af5cf0282354ff5b2b501f4330865d116e4162
trinhtanphat <89531707+trinhtanphat@users.noreply.github.com>
2021-10-18 08:41:31 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,48 @@
+const CONFIG = {
+    introTitle: 'Hee luu cậu!',
+    introDesc: `Tớ có điều này muốn hỏi cậu, nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'oke',
+    title: 'Cậu yêu tớ có phải không 😙',
+    desc: 'Cậu không trả lời hoặc thoát ra, tức là cậu là vợ tớ đó nha :> ',
+    btnYes: 'yêu ơi là iuuu <33',
+    btnNo: 'cậu mơ à :333',
+    question:'Nói cho tới lý do vì sao cậu yêu tớ đii <3',
+    btnReply: 'Gửi cho tớ <3',
+    reply: 'Tại vì cậu đẹp trai hihi <33333333',
+    mess: 'Tớ biết mà 🥰. Love you 300.000 😘😘',
+    messDesc: 'còn giờ thì chờ gì nữa mà không inbox cho tớ đi nào.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
+/*
+# Tặng Crush
+## _Một điều nho nhỏ tỏ tình với crush_
+
+Liên lạc: 
+[![Facebook](https://i.imgur.com/GRqy96ts.jpg)](https://www.facebook.com/nam.nodemy)
+[![Tiktok](https://i.imgur.com/Nbfl1E7t.jpg)](https://www.tiktok.com/@manindev)
+
+Mở file config.js sửa nội dung theo mẫu
+```
+const CONFIG = {
+    introTitle: 'Babe à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
+```
+
+*/
